2c66b83c3bc333b2f92f0643ffe2d8a65850a749
bss4515 <huangwei4515@gmail.com>
2016-06-02 17:55:22 +0800

    code update
    code : linklist

---
+++ Allen/java/code/linkedlistuse/LinkedListUse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package linkedlistuse;
+
+/**
+ *
+ * @User:Huang Yi
+ */
+public class LinkedListUse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Linkedlist list=new Linkedlist();
+        list.add("A");
+        list.add("B");
+      System.out.println(list.get(0));
+    }
+
+}
